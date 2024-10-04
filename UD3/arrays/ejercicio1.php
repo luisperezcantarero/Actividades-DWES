2ee2b69fe6d273241b700954af3233afa7422a3c
luisperezcantarero <a23pecalu@iesgrancapitan.org>
2024-10-04 12:42:23 +0000

    conf y ejercicio1 array

---
+++ UD3/arrays/ejercicio1.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Definir un array que permita almacenar y mostrar la siguiente información. 
+ * a. Meses del año.
+ * b. Tablero para jugar al juego de los barcos.
+ * c. Nota de los alumnos de 2o DAW para el módulo DWES.
+ * d. Verbos irregulares en inglés.
+ * e. Información sobre continentes, países, capitales y banderas.
+ * 
+ * 
+ */
+
+ $meses = array("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre");
+ $mesesCount = count($meses);
+ for($i = 0; $i < $mesesCount; $i++) {
+    echo $meses[$i];
+    echo "<br>";
+ }
+
+ $tableroBarcos = array("A" => array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10),
+                        "B" => array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10),
+                        "C" => array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10),
+                        "D" => array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10),
+                        "E" => array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10),
+                        "F" => array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10),
+                        "G" => array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10),
+                        "H" => array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10),
+                        "I" => array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10),
+                        "J" => array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+);
+ 
+echo "<br>Tablero de Barcos<br>";
+$coordenadas = array();
+foreach ($tableroBarcos as $coordenada) {
+    $coordenadas = $coordenada;
+    print_r($coordenadas);
+    echo "<br>";
+}
+
+echo "<br>Nota Alumnos<br>";
+$notaAlumnos = array( array("nombre" => "Luis", "nota" => 4), array("nombre" => "Jose María", "nota" => 7), array("nombre" => "Carlos", "nota" => 7), array("nombre" => "Marlon", "nota" => 8));
+$notaFinal = array();
+foreach ($notaAlumnos as $nota) {
+    $notaFinal[] = $nota["nota"];
+}
+print_r($notaFinal);
+echo "<br>";
+
+$verbosIrregulares = array( array("Base Form" => array("arise", "get", "shut"), "Past Form" => array("arose", "got", "shut")))
+?>
---
+++ conf/conf.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * 
+ * 
+ * 
+ */
+
+ $actvidades = array("UD3" => array( "condicionales" => array("ej1.php", "ej2.php", "ej3.php", "ej4.php"),
+                                            "bucles" => array("ej1.php", "ej2.php", "ej3.php", "ej4.php"),
+                                            "arrays" => array("ej1.php", "ej2.php", "ej3.php", "ej4.php",
+                                                                "ejemplo1.php", "ejemplo2.php", "ejemplo3.php", "ejemplo4.php")
+                ));
+?>
